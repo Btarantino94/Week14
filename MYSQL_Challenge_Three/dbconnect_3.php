acf3f7ccfeb43e27695073d226fd95096f1eff1d
Btarantino94 <Btarantino94@gmail.com>
2017-09-26 11:27:55 -0400

    db change

--- MYSQL_Challenge_Three/dbconnect_3.php
+++ MYSQL_Challenge_Three/dbconnect_3.php
@@ -1,7 +1,7 @@
  <?php
 
 try{
-  $db = new PDO('mysql:host=localhost;dbname=Challenge_three', 'root', 'root');
+  $db = new PDO('mysql:host=localhost;dbname=btarantino_Challenge_one', 'r2hstudent', 'SbFaGzNgGIE8kfP');
   $db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $db-> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
